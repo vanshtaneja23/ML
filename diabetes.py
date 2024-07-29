be4a78ca21512051e569ce373b64f904401ad6fb
vanshtaneja23 <155813476+vanshtaneja23@users.noreply.github.com>
2024-07-30 01:28:06 +0530

    Create diabetes.py

---
+++ diabetes.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error
+
+diabetes = datasets.load_diabetes()
+diabetes_x= diabetes.data[:,np.newaxis,2]
+diabetes_x_train= diabetes_x[:-30]
+diabetes_x_test= diabetes_x[-20:]
+
+diabetes_y_train= diabetes.target[:-30]
+diabetes_y_test=diabetes.target[-20:]
+
+
+model= linear_model.LinearRegression()
+model.fit(diabetes_x_train,diabetes_y_train)
+
+diabetes_y_predict= model.predict(diabetes_x_test)
+print("Mean squared error ", mean_squared_error(diabetes_y_test,diabetes_y_predict))
+
+print("weights",model.coef_)
+print("Intercept: ",model.intercept_)
+
+
+plt.scatter(diabetes_x_test,diabetes_y_test)
+plt.plot(diabetes_x_test,diabetes_y_predict)
+plt.show()
